5b9aa8e09ef531290dfe396a16fdd208f3fc8f16
Josh <joshhendo@outlook.com>
2016-11-19 16:53:08 +1100

    Using putIfAbsent

--- src/main/java/org/apache/activemq/isolation/lock/Lock.java
+++ src/main/java/org/apache/activemq/isolation/lock/Lock.java
@@ -5,15 +5,21 @@ import java.util.Date;
 
 public class Lock {
     private String lockId;
+    private String guid;
     private Date created;
 
     public Lock(String lockId) {
         this.lockId = lockId;
+        this.guid = java.util.UUID.randomUUID().toString();
         this.created = Calendar.getInstance().getTime();
     }
 
     public String getLockId() {
         return this.lockId;
+    }
+
+    public String getGuid() {
+        return this.guid;
     }
 
     public Date getCreatedDate() {
